8ce92e068815c9cd65033ceeddc1bcc3770f4e19
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 19:12:24 +0530

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -393,33 +393,29 @@ async def demote(_, message: Message):
 
 
 # Pin Messages
-
-
-@app.on_message(filters.command(["pin", "unpin","unpinall"]) & ~filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["pin", "unpin", "unpinall"]) & ~filters.private & ~BANNED_USERS)
 @adminsOnly("can_pin_messages")
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return await message.reply_text("Reply to a message to pin/unpin it.")
     r = message.reply_to_message
     if message.command[0] == "unpinall":
-    	await app.unpin_all_chat_messages(message.chat.id)
+        await app.unpin_all_chat_messages(message.chat.id)
         return await message.reply_text("All messages unpinned in this chat")
     if message.command[0][0] == "u":
         await r.unpin()
         return await message.reply_text(
-            f"**Unpinned [this]({r.link}) message.**",
+            f"Unpinned [this]({r.link}) message.",
             disable_web_page_preview=True,
         )
     await r.pin(disable_notification=True)
     await message.reply(
-        f"**Pinned [this]({r.link}) message.**",
+        f"Pinned [this]({r.link}) message.",
         disable_web_page_preview=True,
     )
     msg = "Please check the pinned message: ~ " + f"[Check, {r.link}]"
     filter_ = dict(type="text", data=msg)
     await save_filter(message.chat.id, "~pinned", filter_)
-
-
 # Mute members
 
 
