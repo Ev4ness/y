88de5a94976ca07c9d63303ed3be67344e340239
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 09:54:58 +0530

    Update error.py

--- utils/error.py
+++ utils/error.py
@@ -47,7 +47,7 @@ def capture_err(func):
         except Exception as err:
             errors = traceback.format_exc()
             error_feedback = split_limits(
-                "**ERROR** | {} | {}\n```python\n{}```\n\n```python\n{}```\n".format(
+                "**ERROR** | {} | {}\n```command\n{}```\n\n```python\n{}```\n".format(
                     0 if not message.from_user else message.from_user.mention,
                     (
                         0
