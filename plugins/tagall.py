7c5d549eb6b0f0b52973134e2c8da75bcabae4ef
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 07:49:55 +0530

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -148,72 +148,6 @@ async def tag_all_admins(_, message):
         except Exception:
             pass
 
-
-@app.on_message(
-    filters.command(
-        ["atag", "aall", "amention", "amentionall"], prefixes=["/", "@", ".", "#"]
-    )
-    & admin_filter
-)
-async def atag_all_useres(_, message):
-    userbot = await get_assistant(message.chat.id)
-    if message.chat.id in SPAM_CHATS:
-        return await message.reply_text(
-            "ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss ɪs ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴛᴏᴘ sᴏ ᴜsᴇ /acancel"
-        )
-    replied = message.reply_to_message
-    if len(message.command) < 2 and not replied:
-        await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@aall Hi Friends`"
-        )
-        return
-    if replied:
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f"[{m.user.first_name}](tg://openmessage?user_id={m.user.id})"
-            if usernum == 14:
-                await userbot.send_message(
-                    message.chat.id,
-                    f"{replied.text}\n\n{usertxt}",
-                    disable_web_page_preview=True,
-                )
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-    else:
-        text = message.text.split(None, 1)[1]
-
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f'<a href="tg://openmessage?user_id={m.user.id}">{m.user.first_name}</a>'
-
-            if usernum == 14:
-                await userbot.send_message(
-                    message.chat.id, f"{text}\n{usertxt}", disable_web_page_preview=True
-                )
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-
-
 @app.on_message(
     filters.command(["admin", "admins"], prefixes=["/", "@"]) & filters.group
 )
@@ -292,11 +226,7 @@ __HELP__ = """
 
 @all ᴏʀ /all | /tagall ᴏʀ  @tagall | /mentionall ᴏʀ  @mentionall [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴜsᴇʀ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙᴛ ʙᴏᴛ
 
-@aall ᴏʀ /aall | /atagall ᴏʀ  @atagall | /amentionall ᴏʀ  @amentionall  [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴜsᴇʀ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ ʙᴏᴛ's ᴀssɪsᴛᴀɴᴛ
-
-
 /admins Oʀ @admins [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴀᴅᴍɪɴ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ
-
 
 
 /cancel Oʀ @cancel |  /offmention Oʀ @offmention | /mentionoff Oʀ @mentionoff | /cancelall Oʀ @cancelall - ᴛᴏ sᴛᴏᴘ ʀᴜɴɴɪɴɢ ᴀɴʏ ᴛᴀɢ ᴘʀᴏᴄᴇss
