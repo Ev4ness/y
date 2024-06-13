13f3bd9aee4b11a677ffff014c775ecd829f527e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-13 19:59:44 +0530

    Update upscale.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- plugins/upscale.py
+++ plugins/upscale.py
@@ -2,22 +2,30 @@ from pyrogram import Client, filters
 from lexica import Client as LexicaClient
 
 from YukkiMusic import app 
+from utils.error import capture_err
 
 lexica_client = LexicaClient()
 
 def upscale_image(image: bytes) -> bytes:
     return lexica_client.upscale(image)
 
-@app.on_message(filters.command("upscale") & filters.reply)
+@app.on_message(filters.command("upscale"))
+@capture_err
 async def upscale_reply_image(client, message):
+    if not message.reply_to_message.photo:
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ....😑")
     if message.reply_to_message.photo:
+        a = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ......")
         photo = await client.download_media(message.reply_to_message.photo.file_id)
         
         with open(photo, 'rb') as f:
             image_bytes = f.read()
-        
-        upscaled_image_bytes = upscale_image(image_bytes)
-        
-        with open('upscaled.png', 'wb') as f:
-            f.write(upscaled_image_bytes)
-            await message.reply_photo(photo='upscaled.png')
+        try:
+            upscaled_image_bytes = upscale_image(image_bytes)
+            await a.edit("ᴀʟᴍᴏsᴛ ᴅᴏɴᴇ......❣️")
+            with open('upscaled.png', 'wb') as f:
+                f.write(upscaled_image_bytes)
+                await message.reply_photo(photo='upscaled.png')
+                await a.delete()
+        except Exception as e:
+            await e.edit(e)
