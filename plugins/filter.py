700cf5eaa4b0fd8cea2bfc7bd73a87d09ca21cc0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-14 18:44:30 +0530

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -19,9 +19,16 @@ from YukkiMusic.utils.functions import (
     extract_text_and_keyb,
     get_data_and_name,
 )
+from YukkiMusic.utils.database import (
+    delete_filter,
+    deleteall_filters,
+    get_filter,
+    get_filters_names,
+    save_filter,
+)
+
 from config import BANNED_USERS
 
-filtersdb = mongodb.filters
 
 __MODULE__ = "Filters"
 __HELP__ = """/filters To Get All The Filters In The Chat.
@@ -39,73 +46,7 @@ You can use markdown or html to save text too.
 
 Checkout /markdownhelp to know more about formattings and other syntax.
 """
-
-async def get_filters_count() -> dict:
-    chats_count = 0
-    filters_count = 0
-    async for chat in filtersdb.find({"chat_id": {"$lt": 0}}):
-        filters_name = await get_filters_names(chat["chat_id"])
-        filters_count += len(filters_name)
-        chats_count += 1
-    return {
-        "chats_count": chats_count,
-        "filters_count": filters_count,
-    }
-
-
-async def _get_filters(chat_id: int) -> Dict[str, int]:
-    _filters = await filtersdb.find_one({"chat_id": chat_id})
-    if not _filters:
-        return {}
-    return _filters["filters"]
-
-
-async def get_filters_names(chat_id: int) -> List[str]:
-    _filters = []
-    for _filter in await _get_filters(chat_id):
-        _filters.append(_filter)
-    return _filters
-
-
-async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    _filters = await _get_filters(chat_id)
-    if name in _filters:
-        return _filters[name]
-    return False
-
-
-async def save_filter(chat_id: int, name: str, _filter: dict):
-    name = name.lower().strip()
-    _filters = await _get_filters(chat_id)
-    _filters[name] = _filter
-    await filtersdb.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"filters": _filters}},
-        upsert=True,
-    )
-
-
-async def delete_filter(chat_id: int, name: str) -> bool:
-    filtersd = await _get_filters(chat_id)
-    name = name.lower().strip()
-    if name in filtersd:
-        del filtersd[name]
-        await filtersdb.update_one(
-            {"chat_id": chat_id},
-            {"$set": {"filters": filtersd}},
-            upsert=True,
-        )
-        return True
-    return False
-
-
-async def deleteall_filters(chat_id: int):
-    return await filtersdb.delete_one({"chat_id": chat_id})
-    
-    
-
-@app.on_message(filters.command("filter") & ~filters.private & ~BANNED_USERS)
+ @app.on_message(filters.command("filter") & ~filters.private & ~BANNED_USERS)
 @adminsOnly("can_change_info")
 async def save_filters(_, message):
     try:
