270703e8b50a41e39e6c15186670caf1efae401a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-17 10:28:18 +0530

    Update plugins

---
+++ plugins/hug.py
@@ -0,0 +1,13 @@
+from YukkiMusic import app 
+from pyrogram import client, filters
+import nekos
+
+@app.on_message(filters.command("hu"))
+async def huggg(client, message):
+    try:
+        if message.reply_to_message:
+            await message.reply_video(nekos.img("hug"), caption=f"{message.from_user.mention} hugged {message.reply_to_message.from_user.mention}")
+        else:
+            await message.reply_video(nekos.img("hug"))
+    except Exception as e:
+        await message.reply_text(f"Error: {e}")
