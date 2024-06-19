9231aeebd451c4a7dcbc026a13cbbdea902cdcf6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-19 11:05:13 +0530

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -192,12 +192,12 @@ async def admintag_with_reporting(client, message):
             chat_id, filter=ChatMembersFilter.ADMINISTRATORS
         )
     ]
-
-    if from_user_id in admins:
-        return await tag_all_admins(client, message)
     if message.command[0] == "report":
         if from_user_id in admins:
             return await message.reply_text("ᴏᴘᴘs! ʏᴏᴜ ᴀʀᴇ ʟᴏᴏᴋs ʟɪᴋᴇ ᴀɴ ᴀᴅᴍɪɴ ᴄᴀɴ ʀᴇᴘᴏʀᴛ ᴀɴʏ ᴜsᴇʀs ᴛᴏ ᴀᴅᴍɪɴ")
+
+    if from_user_id in admins:
+        return await tag_all_admins(client, message)
 
     if len(message.text.split()) <= 1 and not message.reply_to_message:
         return await message.reply_text("Reply to a message to report that user.")
