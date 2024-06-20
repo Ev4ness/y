d255354e8856725cc7f00cbb8dcc4e9947551546
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-20 07:36:31 +0700

    update plugins/advice.py

--- plugins/advice.py
+++ plugins/advice.py
@@ -26,7 +26,7 @@ async def advice(_, message):
         await app.send_message(LOG_GROUP_ID, "/astronomical not working")
 
 
-__MODULE__ = "Aᴅᴠɪᴄᴇ"
+__MODULE__ = "Device"
 __HELP__ = """
 /advice - Gᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ
 /astronomical - ᴛᴏ ɢᴇᴛ ᴛᴏᴅᴀʏ's ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ  ғᴀᴄᴛ"""
