be68896bb89fceeea985f6a5e13c74fd604ffbc6
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-20 07:41:41 +0700

    update plugins/fake.py

--- plugins/fake.py
+++ plugins/fake.py
@@ -46,7 +46,7 @@ async def fkadress(_, message):
         await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.\nᴛʀʏ ᴀɢᴀɪɴ")
 
 
-__MODULE__ = "Fᴀᴋᴇ"
+__MODULE__ = "Fake"
 __HELP__ = """
 
 /fake [ᴄᴏᴜɴᴛʀʏ ɴᴀᴍᴇ ] - ᴛᴏ ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴅʀᴇss"""
